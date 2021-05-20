b70fe133f6ccd7151fdf3d66a34aa0ac46607cb3
Вітя Варварук <50269791+varvaruk-v@users.noreply.github.com>
2021-05-20 13:12:46 +0300

    v.1.2.2

--- crypt.py
+++ crypt.py
@@ -1,7 +1,28 @@
 import pyAesCrypt
 import os
 
-def encrypt(file, password):
+from requests import status_codes
+
+def encrypt(data, key, method):
+    import requests as r
+    enc = r.get(f"https://api.vaar.pp.ua/crypt/?data={data}&key={key}&method={method}&mode=e")
+    status_code = enc.status_code
+    enc = enc.json()
+    if status_code == 200:
+        return enc["text"]
+    else:
+        return "[Error] "+str(enc["message"])
+
+def decrypt(data, key, method):
+    import requests as r
+    enc = r.get(f"https://api.vaar.pp.ua/crypt/?data={data}&key={key}&method={method}&mode=d")
+    status_code = enc.status_code
+    enc = enc.json()
+    if status_code == 200:
+        return enc["text"]
+    else:
+        return "[Error] "+str(enc["message"])
+def EncryptFile(file, password):
     bufferSize = 512*1024
     pyAesCrypt.encryptFile(
         str(file),
@@ -16,7 +37,7 @@ def encrypt(file, password):
         os.remove(file)
         print("[INFO]: Original file was deleted")
 
-def decrypt(file, password):
+def DecryptFile(file, password):
     bufferSize = 512*1024
     try:
         pyAesCrypt.decryptFile(
@@ -34,32 +55,58 @@ def decrypt(file, password):
         print("[Error]: Wrong password or file is damaged!")
 
 os.system('cls||clear')
-print("""
+print("""                                                                    
                                                                                                     
                                                                                                     
-     `./osyyso+:.                                                :ooooooooo   ``   /o/        v.1.2.1     
-   `odMNdyssyhmNN-                                               sMMyyyyyyy  +mh   hMy              
-  :mMm/`       .:.                                       ./      sMM`        .+:   hMy              
- -NMh`              /+- -oo/``/+-     :+/ .+++++/-`    .sNm+++-  sMM`        -+/   hMy    `:+oo+-   
- yMM`               dMyyhydN/ +MN-   :MM/ /MMhhhdNNy. -ymMNyyy:  sMMddddddd` oMd   hMy   /mNhoohMh. 
- yMM`               dMm.   `   +MN- -NMo  /MM`   `sMN`  yMh      sMM+//////  oMd   hMy  :MN-    oMd 
- :MMs               dMs         oMN:NMs   /MM`     MM+  yMh      sMM`        oMd   hMy  sMNhhhhhhhh`
-  /NMh:         --  dMo          oMMMy    /MM`    /MM-  yMh      sMM`        oMd   hMy  /MM-      ` 
-   .yNMNhsoooydNM+  dMo          `NMy     /MMo//+yMN/   /MNo/+y  sMM`        oMd   hMy   +NMho++shm 
-     `:osyhhyyo/.   os/         `dMh`     /MMsyhys/`     :shhy+  /ss         :so   +s/    `+yhhys+. 
-                                hMd`      /MM`                                                      
-                               yMm.       /MM`                                                      
-                              :yy.        -yy`                                                      
-                                                                                                    
+       ```````                                                    ```                         v.1.2.2      
+    .ohdmNNNmdy/`                                                sddd`                              
+  `sMMMMMNNNMMMMm:   ```  ````  ````     `````  ```   ````     `-MMMM.``     ```````      ````  ``` 
+  sMMMMm:``.+MMMMN. -mmmh/dMMd -mmmmy   `dmmmo ommmoodNMMNh:  /mNMMMMmmy   :ydNMMMNdy:   `mmmd/dNMN 
+  NMMMM/     +ssss. -MMMMMMMMm  oMMMM:  +MMMm` sMMMMMdhmMMMM+ :hNMMMMhhs `hMMMNysyNMMMh` `MMMMMMMMN 
+ `MMMMM:            -MMMMN/..`   hMMMm `NMMM-  sMMMM/   dMMMN   yMMMM`   +MMMM:   :MMMM+ `MMMMN+..` 
+  mMMMMs     sNNNN- -MMMMs       `mMMM+oMMMo   sMMMM.   hMMMM   yMMMM`   sMMMM.   .MMMMs `MMMMy     
+  :MMMMMh+/+hMMMMd  -MMMMo        -NMMNNMMh    sMMMMh--/NMMMd   yMMMM:`  :MMMMs.`.sMMMM- `MMMMy     
+   -hMMMMMMMMMMNs`  -MMMMo         /MMMMMN.    sMMMMMMMMMMMm.   +MMMMMMh  :mMMMMMMMMMd:  `MMMMy     
+     `:osyyss+:`    `oooo:          dMMMM/     sMMMM:/syso:`     -osyys/    -+ssyss+-    `oooo:     
+                                /yydMMMN+      sMMMM-                                               
+                                omNNNds.       +dddd.                                               
+                                 `````          ````                                                
                                                                                                     
 """)
+try:
+    FileOrText = input("[+] What do yo want to crypt? (Text/File) ").lower()
+    if FileOrText == "text" or FileOrText == "t":
+        mode = input("[+] Do you want encrypt or decrypt text? (E/D) ").lower()
+        data = input("[+] Enter text: ")
+        print("--------------------")
+        password = input("[+] Enter password: ")
+        if mode == "e" or mode == "en" or mode == "encrypt":
+            encrypted = encrypt(data, password, "AES-256-CBC")
+            if "[Error]" in encrypted:
+                print(encrypted)
+            else:
+                print("Encrypted text: "+str(encrypted))
+        elif mode == "d" or mode == "de" or mode == "decrypt":
+            decrypted = decrypt(data, password, "AES-256-CBC")
+            if "[Error]" in decrypted:
+                print(decrypted)
+            else:
+                print("Decrypted text: "+str(decrypted))
+        else:
+            print("[Error] Unknown mode")
+    elif FileOrText == "file" or FileOrText == "f":
+        mode = input("[+] Do you want encrypt or decrypt file? (E/D) ").lower()
+        dir = input("[+] Enter file: ")
+        print("--------------------")
+        password = input("[+] Enter password: ")
 
-mode = input("[+] Do you want encrypt or decrypt file? (E/D) ").lower()
-dir = input("[+] Enter file: ")
-print("--------------------")
-password = input("[+]Enter password: ")
-
-if mode == "e" or mode == "en" or mode == "encrypt":
-    encrypt(dir, password)
-elif mode == "d" or mode == "de" or mode == "decrypt":
-    decrypt(dir, password)
+        if mode == "e" or mode == "en" or mode == "encrypt":
+            EncryptFile(dir, password)
+        elif mode == "d" or mode == "de" or mode == "decrypt":
+            DecryptFile(dir, password)
+        else:
+            print("[Error] Unknown mode")
+    else:
+        print("[Error] Unknown mode")
+except:
+    pass
