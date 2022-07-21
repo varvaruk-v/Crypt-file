aa3b073e2054eb0ce753b5e2304ffc227d2959b9
Вітя Варварук <viktorvarvaruk01@gmail.com>
2022-07-21 15:36:14 +0100

    v.1.5.6

--- cryptortool/cli.py
+++ cryptortool/cli.py
@@ -7,7 +7,7 @@ def logo():
     os.system('cls||clear')
     print("""                                                                    
                                                                                                                                                                                                         
-           ```````                                                    ```                         v.1.5.5      
+           ```````                                                    ```                         v.1.5.6      
         .ohdmNNNmdy/`                                                sddd`                              
       `sMMMMMNNNMMMMm:   ```  ````  ````     `````  ```   ````     `-MMMM.``     ```````      ````  ``` 
       sMMMMm:``.+MMMMN. -mmmh/dMMd -mmmmy   `dmmmo ommmoodNMMNh:  /mNMMMMmmy   :ydNMMMNdy:   `mmmd/dNMN 
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ URL = "https://github.com/varvaruk-v/Crypt-file"
 EMAIL = ""
 AUTHOR = "Viktor Varvaruk"
 REQUIRES_PYTHON = ">=3.7.0"
-VERSION = "1.5.5"
+VERSION = "1.5.6"
 
 with open("requirements.txt", encoding="utf-8") as f:
     REQUIRED = f.readlines()
@@ -29,7 +29,7 @@ setup(
     url=URL,
     packages=["cryptortool"],
     entry_points={
-        "console_scripts": ["cryptor=cryptortool.cli:main", "cryptortool=cryptortool.cli:main", "hash=cryptortool.hash:main"]
+        "console_scripts": ["cryptor=cryptortool.cli:main", "cryptortool=cryptortool.cli:main", "hash=cryptortool.hash:main", "chash=cryptortool.hash:main"]
     },
     install_requires=REQUIRED,
     license="MIT",
